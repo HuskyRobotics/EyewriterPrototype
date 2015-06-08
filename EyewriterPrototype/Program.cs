95ea72e18f8002b8fee1c470ba3243239feff6bf
Silver <silvertorch5@gmail.com>
2015-06-08 15:38:14 -0600

    Fixes

--- EyewriterPrototype/Program.cs
+++ EyewriterPrototype/Program.cs
@@ -1,9 +1,10 @@
-﻿using Graphics.UserInterface;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Utilities;
 
 namespace Eyewriter_Prototype
 {
